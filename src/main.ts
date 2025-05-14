1e00f10579007813eb9e28852507a929d2d4eba4
ash-digit <ashdigit44@gmail.com>
2025-05-14 11:48:58 +0100

    first commit with all dependencies

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import "./style.scss";
+
+import axios from "axios";
+
+// axios
+//   .get("http://localhost:8080/api/customers/2")
+//   .then((response) => {
+//     console.log("GET posts:", response.data);
+//   })
+//   .catch((error) => {
+//     console.error("GET error:", error);
+//   });
+
+const msg1 = async () => {
+  //Async Arrow Function
+  try {
+    const msg = await axios.get("http://localhost:8080/api/customers/2");
+    return msg;
+  } catch (err) {
+    console.log("error occured! " + err);
+  }
+};
+
+console.log(await msg1());
