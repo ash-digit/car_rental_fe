50f6f40983752cefc7731d7dde079314b365ef00
ash-digit <ashdigit44@gmail.com>
2025-05-14 16:27:47 +0100

    refactored the carGenerator function

--- src/main.ts
+++ src/main.ts
@@ -22,18 +22,35 @@ const populateCars = async () => {
 
 const carGenerator = (car: Car) => {
   const card = document.createElement("div");
+
+  const innerContent = `<div class="car" id="${car.id}">
+        <div class="car__image">
+          <img src="https://placehold.co/300x200" alt="placeholder" />
+        </div>
+        <div class="car__info">
+          <p class="car__info car__info--make">${car.make} ${car.model}</p>
+          <p class="car__info car__info--year">${car.year}</p>
+          <p class="Car.">£${car.dailyRate}</p>
+          <p class="car__info car__info--availability">${car.availability}</p>
+        </div>
+        <button class="car__prefill">Prefill</button>
+      </div> `;
+  card.innerHTML = innerContent;
   carInventory.appendChild(card);
-  card.classList.add("car");
-  card.id = car.id.toString();
-  const carImage = document.createElement("div");
-  carImage.classList.add("car__image");
-  const image = document.createElement("img");
-  image.setAttribute("src", "https://placehold.co/300x200");
-  carImage.appendChild(image);
-  card.appendChild(carImage);
-  const test = document.createElement("p");
-  test.innerHTML = car.model;
-  card.appendChild(test);
+  // carInventory.appendChild(card);
+  // card.classList.add("car");
+  // card.id = car.id.toString();
+  // const carImage = document.createElement("div");
+  // carImage.classList.add("car__image");
+  // const image = document.createElement("img");
+  // image.setAttribute("src", "https://placehold.co/300x200");
+  // carImage.appendChild(image);
+  // card.appendChild(carImage);
+  // const test = document.createElement("p");
+  // test.innerHTML = car.model;
+  // card.appendChild(test);
+
+  //
 
   //     card.innerHTML = `<p>Name:${char.name}</p>
   //                 <img src="${char.image}" alt="Image of ${char.name}">
